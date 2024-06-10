e427b19e15b95c6bc6545fc09d745349ca0193c6
Arturoo55 <bussinesarturo30@gmail.com>
2024-06-10 17:02:07 +0200

    Add files via upload

---
+++ Prinicpal.java
@@ -0,0 +1,15 @@
+
+
+public class Prinicpal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//segunda fase
+		
+		
+		
+		Clase clase= new Clase();
+	}
+
+}
